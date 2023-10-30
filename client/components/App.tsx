475a0ed1ecfd02c7921b5c2b14563703b9b4ecd0
Joseph Quested <josephquested@gmail.com>
2023-10-31 11:56:32 +1300

    react the unicorns

---
+++ client/components/App.tsx
@@ -0,0 +1,13 @@
+import Unicorn from './Unicorn.tsx'
+
+function App() {
+  return (
+    <div>
+      <Unicorn />
+      <Unicorn />
+      <Unicorn />
+    </div>
+  )
+}
+
+export default App
---
+++ client/components/Unicorn.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import img0 from '../images/unicorn-0.png'
+import img1 from '../images/unicorn-1.png'
+import img2 from '../images/unicorn-2.png'
+import img3 from '../images/unicorn-3.png'
+
+const images = [img0, img1, img2, img3]
+
+function Unicorn() {
+
+    const [inflation, setInflation] = useState(0)
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setInflation((inflation) => inflation < 3 ? inflation + 1 : 0)
+    }
+    
+    return (
+        <img src={images[inflation]} onClick={handleClick} />
+    )
+}
+
+export default Unicorn
